61eb4d4224f6c936ddbd02988dc608a3d4cd9eb1
Korbin Mays <Tito@Reginas-MacBook-Pro.local>
2020-04-22 05:27:02 -0500

    added files and folders

---
+++ api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "5046bd48dfaf4edbd3daa6d83fdfad30"
+
+# Google API Key
+g_key = "AIzaSyCyeuEUOS_MqYp5zEtoqm23voGtddZa0bs"
+
+# Census API Key
+census_key = "03f8e3c8e009f7e6fcc0f922380a1c6bfdffbaee"
